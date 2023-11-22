b927b33fb2123498fbd5e22bfa89ada2a0d8c14f
AmatoCaldas <joao.amato015@gmail.com>
2023-11-22 20:12:36 -0300

    Adiciona funcionalidades a função

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 let maria = 2;
 let joao = 1;
 
-function deJoaoParaMaria() {}
+function setMaria() {
+  maria = 1;
+}
 
-function deMariaParaJoao() {}
+function setJoao() {
+  joao = 1;
+}
+
+function deJoaoParaMaria() {
+  maria = maria + joao;
+  joao = 0;
+}
+
+function deMariaParaJoao() {
+  joao = joao + maria;
+  maria = 0;
+}
 
 function getMaria() {}
 
