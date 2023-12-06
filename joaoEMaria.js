475a1fe5ecbc422896ac65cdada92789e080f262
AmatoCaldas <joao.amato015@gmail.com>
2023-12-06 09:29:34 -0300

    adiciona resolução dos testes

---
+++ joaoEMaria.js
@@ -0,0 +1,43 @@
+let maria = 2;
+let joao = 1;
+{
+  let joao = 3;
+}
+function getMaria() {
+  return maria;
+}
+function getJoao() {
+  return joao;
+}
+function setMaria(num) {
+  if (num >= 0) {
+    maria = num;
+  } else {
+    maria = 0;
+  }
+}
+function setJoao(num) {
+  if (num >= 0) {
+    joao = num;
+  } else {
+    joao = 0;
+  }
+}
+function deJoaoParaMaria() {
+  maria = joao + maria;
+
+  joao = 0;
+}
+function deMariaParaJoao() {
+  joao = joao + maria;
+
+  maria = 0;
+}
+export {
+  getJoao,
+  getMaria,
+  setMaria,
+  setJoao,
+  deMariaParaJoao,
+  deJoaoParaMaria,
+};
---
+++ main.js
@@ -0,0 +1,14 @@
+import { getMaria, getJoao } from './joaoEMaria.js';
+import { deJoaoParaMaria, setJoao, setMaria } from './joaoEMaria.js';
+
+setMaria(4);
+setJoao(-2);
+
+let maria = getMaria();
+let joao = getJoao();
+
+deJoaoParaMaria();
+
+maria = getMaria();
+joao = getJoao();
+console.log(maria, joao);
